1b41da4498b1d35f5e2c43d60d32f7d5d9bde5df
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-16 13:44:14 +0200

    Adding a script

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -9,6 +10,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+        if (n>0)
+    {
+        printf("The number is positive \n");
+    }
+        else if (n<0)
+    {
+        printf("The number is negative \n");
+    }
+        if (n==0)
+    {
+        printf("The number is zero \n");
+    }
+
+            
+
 	return (0);
 }
