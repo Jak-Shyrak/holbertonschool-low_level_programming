d98390b52a02b1e4e7e620d850f40da2acf99e50
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-16 16:28:32 +0200

    Adding a script

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,11 @@
 */
 int main(void)
 {
-    char a;
-
-    for (a = 97; a <= 122; a++)
-        putchar(a);
-    for (a = 65; a <= 90; a++)
-        putchar(a);
-    putchar('\n');
-    return (0);
+char a;
+for (a = 97; a <= 122; a++)
+putchar(a);
+for (a = 65; a <= 90; a++)
+putchar(a);
+putchar('\n');
+return (0);
 }
