f819b0e2a3fbe9dde9636446187a4248f20e400f
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-16 14:18:31 +0200

    Adding a script

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 
-/* Pick a random number.
-if the number is positive print n is positive
-if the number is negative print n is negative
-if the number equal to 0 print n is zero
-*/
+/*Pick a random number.*/
+
+##
 int main(void)
 {
 	int n;
@@ -15,15 +13,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", n); /*if the number is positive print n is positive*/
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", n); /*if the number is negative print n is negative*/
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", n); /*if the number equal to 0 print n is zero*/
 	}
 	return (0);
 }
