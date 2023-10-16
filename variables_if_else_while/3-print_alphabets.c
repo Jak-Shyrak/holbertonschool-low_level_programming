e6d10ffa766e6d5cd3faacbbfa1246ab28df3c30
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-16 16:25:50 +0200

    Adding a script

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-
 /**
 * main - This program
 * print the alphabet
-* in lowercase
+* in lowercase then
+* in uppercase
 * Return: 0
 */
 int main(void)
 {
-char alphabet = 'a';
-while (alphabet <= 'z')
-{
-putchar(alphabet);
-alphabet++;
-}
-putchar('\n');
-return (0);
+    char a;
+
+    for (a = 97; a <= 122; a+)
+        putchar(a);
+    for (a = 65; a <= 90; a+)
+        putchar(a);
+    putchar('\n');
+    return (0);
 }
