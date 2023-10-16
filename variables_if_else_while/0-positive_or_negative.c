3eff2287238f40a68d543783c880c13597f46f55
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-16 13:53:59 +0200

    Adding a script

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,18 +12,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
         if (n>0)
     {
-        printf("The number is positive \n");
+        printf("%d is positive\n", n);
     }
         else if (n<0)
     {
-        printf("The number is negative \n");
+        printf("%d is negative\n", n);
     }
         if (n==0)
     {
-        printf("The number is zero \n");
+        printf("%d is zero\n", n);
     }
-
-            
-
 	return (0);
 }
