ce72ba7019c2e6027b698e8d3d6e2c2d013fb82b
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-16 16:39:50 +0200

    Adding a script

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,8 @@
 /**
 * main - This program
 * print the alphabet
-* in lowercase
+* in lowercase but he
+* don't print e and q
 * Return: 0
 */
 int main(void)
@@ -11,7 +12,11 @@ int main(void)
 	char a;
 
 	for (a = 97; a <= 122; a++)
+	{
+		if (a != 101 && a != 113)
 		putchar(a);
+	}
 	putchar('\n');
+
 	return (0);
 }
