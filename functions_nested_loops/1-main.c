2e5ed0b6e4a49e032225628aedec2b08de1e7968
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-17 15:06:28 +0200

    Adding a script

---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet
+ *
+ * Description: null
+ * Return: always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - This program
+* print the alphabet
+* 10 times
+* in lowercase
+* Return: 0
+*/
+int main()
+{
+     int i;
+	 char a;
+     for(i=0; i<10; i++)
+     {
+        for (a = 97; a <= 122; a++)
+		putchar(a);
+		putchar('\n');
+     }
+     return 0;
+}
+
