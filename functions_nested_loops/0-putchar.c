a285d4269d7394963f8df7ae8fc5c1b76564b8e5
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-17 08:48:42 +0200

    Adding a script

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - This program
+* will assign a random number
+* to the variable n
+* Return: 0
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
