5a34a6ea6dbf2b2b02110f296758f5e0a588754f
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-17 09:05:14 +0200

    Adding a script

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - This program
+* will assign a random number
+* to the variable n
+* Return: 0
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 97; a <= 122; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
