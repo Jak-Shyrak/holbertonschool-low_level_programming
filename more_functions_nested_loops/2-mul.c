cf39bfdd135ea87b3a23b988cffcdb77852fb010
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-19 14:31:52 +0200

    Adding a script

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* mul - multiplies two integers
+* @a: integer one
+* @b: integer two
+* Return: multiplication of a and b
+*/
+int mul(int a, int b);
+{
+	return (a * b);
+}
