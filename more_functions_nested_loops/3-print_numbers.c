027b5715b3a8a5807a5bf83489a609fa57e2f6d4
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-19 16:32:51 +0200

    Adding a script

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 *print_numbers - print the numbers, from 0 to 9
 *Return: void
 */
+
 void print_numbers(void)
 {
-	char n;
+	char c;
 
-	for (n = 0; n <= 9; n++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		putchar(n);
+	_putchar(c);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
