1c3ebcd43f280f6964f84581b420ed6231249977
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-25 14:19:13 +0200

    Adding a script

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,22 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
+
+/**
+ * _strcat - concatenate two strings
+ * @dest : pointer
+ * @src : pointer 2
+ * Return: Always 0.
+ */
 char *_strcat(char *dest, char *src)
 {
+	int a, b;
 
+	for (a = 0; dest[a] != '\0'; a++)
+		;
+	for (b = 0; src[b] != '\0'; b++)
+	{
+		dest[a + b] = src[b];
+	}
+	return (dest);
 }
