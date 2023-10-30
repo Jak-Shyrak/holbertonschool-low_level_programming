0c9da494e0f22a76f73b591b1c30488b355993d6
JakShyrak <bourhlef.yacine53@gmail.com>
2023-10-30 12:56:01 +0100

    Adding a script

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - Imprime une chaîne de caractères en ordre inverse.
+ * @s: La chaîne de caractères à imprimer en ordre inverse.
+ */
+void _print_rev_recursion(char *s)
+{
+    if (*s == '\0') /* Si nous atteignons la fin de la chaîne, nous nous arrêtons.*/
+        return;
+
+    _print_rev_recursion(s + 1); /* Appel récursif avec le caractère suivant.*/
+
+    putchar(*s); /* Imprime le caractère actuel (à partir de la fin).*/
+}
