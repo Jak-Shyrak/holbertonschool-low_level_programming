a5eb3ed664317c3b481e767edcb7213cd2c86fb0
JakShyrak <bourhlef.yacine53@gmail.com>
2023-11-02 11:53:29 +0100

    Adding a script

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    (void)argc;
-    printf("%s\n", argv[0]);
-    return 0;
+/**
+ * main -  a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 SUCCESS
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
