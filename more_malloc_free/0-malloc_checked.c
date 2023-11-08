5face6a801068f198c10a136554c0b974ee656d8
Jak-Shyrak <bourhlef.yacine53^gmail.com>
2023-11-08 05:14:20 -0800

    Adding a Script

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,25 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-
 /**
- *malloc_checked - allocates memory using malloc
- * @b: amount of memory
- * Return: Always 0.
+ * malloc_checked - allocates memory
+ * @b: number of bytes to allocate
+ * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
-	int *mallocated; /*Pointer to allocated memory*/
+	char *allmemo;
 
-	mallocated = malloc(b);
-
-	if (mallocated == NULL)
-	{
-		/*Failed*/
-		printf("Memory allocation failed\n");
-		exit(98);
-	}
-	else
-		return (mallocated); /*Success*/
+	allmemo = malloc(b);/*Allocate memory specified by (b)*/
+	if (allmemo == NULL)
+		exit(98);/*Terminates immediately program with 98 to signal error */
+	return (allmemo);
 }
