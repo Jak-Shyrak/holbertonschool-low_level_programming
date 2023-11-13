0d76a21f71616617533dc872a90e3ad54ccf1305
Jak Shyrak <bourhlef.yacine53@gmail.com>
2023-11-13 13:23:02 +0100

    Adding a Script

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,14 +2,14 @@
 #include "dog.h"
 /**
   * init_dog - initialize struct dog.
-  * @dog: nom de la structure
+  * @d: nom de la structure
   * @name: nom du chien
   * @age: age du chien
   * @owner: nom du propriétaire
   */
-void init_dog(struct dog *dog, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
