1aee94c614a534960382ede06d5e3c4186942ba2
Jak Shyrak <bourhlef.yacine53@gmail.com>
2023-11-13 11:29:52 +0100

    Adding a Script

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: The
+ *
+ * Description: Longer description
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+#endif /* _SAMPLE_HEADER_H_ */
