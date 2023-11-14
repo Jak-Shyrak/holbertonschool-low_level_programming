861c22b8d8cbfe91aea63c99e19a2c36155f24df
Jak Shyrak <bourhlef.yacine53@gmail.com>
2023-11-14 13:40:12 +0100

    Adding a Script

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
+
 /**
-  * print_dog - prints the dog struct
-  * @d: pointer to the dog struct
-  */
-void print_dog(struct dog *d)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	if (d != NULL)
-	{
-		if (d->name != NULL)
-		{
-			printf("Name: %s\n", d->name);
-		} else
-		{
-			printf("Name: (nil)\n");
-		}
-		printf("Age: %f\n", d->age);
-		if (d->owner != NULL)
-		{
-			printf("Owner: %s\n", d->owner);
-		} else
-		{
-			printf("Owner: (nil)\n");
-		}
-	}
+	dog_t *my_dog;
+
+	my_dog = new_dog("Poppy", 3.5, "Bob");
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+	return (0);
 }
---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ *new_dog - Create a new dog
+ *@name: name of the new dog
+ *@age: age of the dog
+ *@owner: Owner of the new dog
+ *Return: new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	/*Allocate memory for the new dog structure*/
+	dog_t *newDog = malloc(sizeof(dog_t));
+
+	/*Check if memory allocation was successful*/
+	if (newDog == NULL)
+		return (NULL);  /*Return NULL if allocation fails*/
+
+/* Allocate memory for the name string and copy the provided value */
+	newDog->name = strdup(name);
+
+	/* Check if name string allocation was successful */
+	if (newDog->name == NULL)
+	{
+		/* Free previously allocated memory for the structure */
+		free(newDog);
+		return (NULL); /* Return NULL if string allocation fails */
+	}
+
+	/* Allocate memory for the owner string and copy the provided value */
+	newDog->owner = strdup(owner);
+
+	/* Check if owner string allocation was successful */
+	if (newDog->owner == NULL)
+	{
+		/*Free previously allocated memory for the structure and name */
+		free(newDog->name);
+		free(newDog);
+		return (NULL); /* Return NULL if string allocation fails */
+	}
+
+	newDog->age = age; /* Copy the provided age value */
+
+	return (newDog); /* Return the pointer to the newly created dog structure */
+}
