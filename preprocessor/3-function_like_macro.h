a544a1f9c0285e24df5716a59526ff4a51358d3b
jakshyrak <bourhlef.yacine53@gmail.com>
2023-11-24 15:38:42 +0100

    Add a function

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _ABS_HEADER
+#define _ABS_HEADER
+#ifndef ABS
+#define ABS(x) ((x) < 0 ? ((x) * (-1)) : (x))
+#endif
+#endif
---
+++ preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef 4_SUM_H
+#define 4_SUM_H
+#ifndef SUM
+#define SUM(x, y) ((x) + (y))
+#endif
+#endif /* 4_SUM_H */
