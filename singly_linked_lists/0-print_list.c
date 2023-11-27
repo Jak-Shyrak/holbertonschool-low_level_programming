4d8c642f13aedf2c39f7a622860ca4fb70c2422b
jakshyrak <bourhlef.yacine53@gmail.com>
2023-11-27 12:02:17 +0100

    Add scripts for the 1st task

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_list - print a list hen she's not empty
+ * @h: elements of list
+ * Return: size of the list
+*/
+size_t print_list(const list_t *h)
+{
+	int size = 0;
+	const list_t *p = h;
+
+	while (p != NULL)
+	{
+		if (p->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			p = p->next;
+			size++;
+		}
+		else
+		{
+			printf("[%d] %s\n", p->len, p->str);
+			p = p->next;
+			size++;
+		}
+	}
+
+	return (size);
+}
